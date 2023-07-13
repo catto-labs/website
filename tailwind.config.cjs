2f059d71a5f4a2664c3caa5edc91990f5b95d598
jason <jason@pnxl.dev>
2023-07-13 22:30:40 +0700

    chore: formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    fontFamily: {
-      sans: ["Red Hat Text Variable", ...defaultTheme.fontFamily.sans],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        fontFamily: {
+            sans: ["Red Hat Text Variable", ...defaultTheme.fontFamily.sans],
+        },
+        extend: {},
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
